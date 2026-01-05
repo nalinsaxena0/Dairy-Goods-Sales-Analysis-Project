095128c262b9b8b9f3f6f5015fd4dfd84034de13
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-06 03:57:06 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,1 +1,55 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	BEGIN TRY
+		PRINT '==================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '==================================';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.dairy_info';
+		TRUNCATE TABLE bronze.dairy_info;
+		PRINT '>> Inserting Data Into: bronze.dairy_info'
+		BULK INSERT bronze.dairy_info
+		FROM 'D:\SQL files\dairy_dataset.csv'
+		WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+		SET @end_time = GETDATE();
+
+		PRINT '==================================';
+		PRINT 'Loading Bronze Layer is completed';
+		PRINT '		- Total Load Douration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '==================================';
+	END TRY
+	BEGIN CATCH
+	PRINT '==================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+	PRINT 'Error Message' + ERROR_MESSAGE();
+	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+	PRINT '==================================';
+	END CATCH
+END
