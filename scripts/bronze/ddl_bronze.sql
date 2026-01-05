283b6173685269c09fe9b6a0a81b3faae339c1ea
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-06 03:29:10 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -15,28 +15,28 @@ GO
 
 CREATE TABLE bronze.dairy_info (
 Location			NVARCHAR(50),
-total_land_area		INT,
+total_land_area		FLOAT,
 number_of_cows		INT,
 farm_size			NVARCHAR(50),
 date				DATE,
 product_id			INT,
 product_name		NVARCHAR(50),
 brand				NVARCHAR(50),
-quantity			INT,
-price_per_unit		INT,
-total_value			INT,
+quantity			FLOAT,
+price_per_unit		FLOAT,
+total_value			FLOAT,
 shelf_life			INT,
 storage_condition	NVARCHAR(50),
 production_date		DATE,
 Expiration_date		DATE,
 quantity_sold		INT,
-selling_price		INT,
-revenue				INT,
+selling_price		FLOAT,
+revenue				FLOAT,
 customer_location	NVARCHAR(50),
 sales_channel		NVARCHAR(50),
 stk_quantity		INT,
-min_stk_threshold	INT,
-reorder_quantity	INT
+min_stk_threshold	FLOAT,
+reorder_quantity	FLOAT
 );
 GO
 
